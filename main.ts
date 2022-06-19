05f83f4d44f8a900543e48e1e271ebbffefedb2d
pirateofspace <94983397+pirateofspace@users.noreply.github.com>
2022-06-19 17:18:00 +0200

    LED controls, receive message

--- main.ts
+++ main.ts
@@ -1,8 +1,101 @@
 radio.onReceivedNumber(function (receivedNumber) {
     power = receivedNumber
+    controlLeds()
+    motors.motorPower(power)
 })
+function controlLeds () {
+    if (power > 0 && power <= 10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (power > 10 && power <= 25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # # . . .
+            `)
+    } else if (power > 25 && power <= 50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+    } else if (power > 50 && power <= 75) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            # # # # .
+            `)
+    } else if (power > 75 && power <= 100) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    } else if (power < 0 && power >= -10) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (power < -10 && power >= -25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # .
+            . . . # #
+            `)
+    } else if (power < -25 && power >= -50) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # # .
+            . . # # #
+            `)
+    } else if (power < -50 && power >= -75) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # # #
+            `)
+    } else if (power < -75 && power >= -100) {
+        basic.showLeds(`
+            # . . . .
+            # # . . .
+            # # # . .
+            # # # # .
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+}
 let power = 0
 radio.setGroup(138)
 basic.forever(function () {
-    motors.motorPower(power)
+	
 })
